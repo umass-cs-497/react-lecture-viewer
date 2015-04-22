07be07ed028beaa39af42d12f241e4512689cf30
Thai Nguyen <thainguyen@umass.edu>
2015-04-22 04:53:51 -0400

    add modal to edit course information

--- src/js/components/courses/CourseEdit.js
+++ src/js/components/courses/CourseEdit.js
@@ -1,39 +1,74 @@
 var React                 = require('react');
 var CourseActionCreator   = require('../../actions/CourseActionCreator');
-var Course = require('./Course');
+var CourseStore          = require('../../stores/CourseStore');
+
+var Modal                 = require('react-bootstrap').Modal;
+var ModalTrigger          = require('react-bootstrap').ModalTrigger;
+var Button                = require('react-bootstrap').Button;
+var OverlayMixin          = require('react-bootstrap').OverlayMixin;
 
 var CourseEdit = React.createClass({
 
   displayName: 'CourseEdit',
 
-  propTypes: {
-    course:      React.PropTypes.shape({
-      id:                   React.PropTypes.number.isRequired,
-      department:           React.PropTypes.string.isRequired,
-      department_shorthand: React.PropTypes.string.isRequired,
-      course_name:          React.PropTypes.string.isRequired,
-      course_number:        React.PropTypes.string.isRequired,
-      description:          React.PropTypes.string.isRequired,
-      section:              React.PropTypes.string.isRequired,
-      term:                 React.PropTypes.string.isRequired,
-      year:                 React.PropTypes.number.isRequired,
-      instructor_id:        React.PropTypes.string.isRequired
-    }).isRequired
+  mixins: [OverlayMixin],
+
+  propTypes : {
+    course_id:  React.PropTypes.number.isRequired
+  },
+
+  getDefaultProps : function() {
+    return {
+      course_id: 1
+    };
   },
 
   getInitialState: function() {
   	return {
+      isModalOpen: false,
   		isEditingInfo: false,
   		isEditingRoster: false,
       isUploadingCsv: false
   	};
   },
 
-  /*============================== @HANDLING ==============================*/
+  /*============================== @LIFECYCLE ==============================*/
 
-  handleEditInformationClick: function() {
-		this.setState({isEditingInfo: true});
-	},
+  componentDidMount : function() {
+    this.contextDidChange(this.props);
+  },
+
+  componentWillReceiveProps : function(nextProps) {
+    this.setState(this.getStateFromStores(nextProps));
+    this.contextDidChange(nextProps);
+  },
+
+  getStateFromStores : function(props) {
+    var course = CourseStore.getCourse(props.course_id);
+    if(!course || course == null) {
+      lecture = {
+        id: null,
+        department: null,
+        course_number: null,
+        section: null,
+        term: null,
+        year: null,
+        description: null
+      }
+    }
+    return { course: course };
+  },
+
+  contextDidChange : function(props) {
+    CourseActionCreator.requestCourse(props.course_id);
+  },
+
+  /*============================== @HANDLING ==============================*/
+  handleToggle: function() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  },
 
 	handleEditRosterClick: function() {
 		this.setState({isEditingRoster: true});
@@ -55,28 +90,36 @@ var CourseEdit = React.createClass({
 
   render: function() {
     return (
-      <div class='course_edit'>
-        {this.renderCourseInfo()}
-        {this.renderInfoEditButton()} <br/>
-        {this.renderRosterEditButton()} <br/><br/>
-      </div>
+      <a onClick={this.handleToggle}>Edit</a>
     )
   },
 
-  renderCourseInfo : function(){
-    var course = this.props.course;
-    if (this.state.isEditingInfo){
-      return <form>
-              Department <input type="text" name="course__deparment" value={course.department}/> <br/>
-              Number <input type="text" name="course__number" value={course.course_number}/>
-              Section <input type="text" name="course__section" value={course.section}/> <br/>
-              Term <input type="text" name="course__term" value={course.term}/>
-              Year <input type="text" name="course__year" value={course.year}/> <br/>
-              Description <br/> <textarea type="text" name="course__description" value={course.description}/> <br/>
-            </form>;
-    } else {
-      return <Course course={course}/>;
+  renderOverlay: function() {
+    if (!this.state.isModalOpen) {
+      return <span/>;
     }
+
+    var course = this.state.course;
+
+    return (
+      <Modal title='Course Edit' onRequestHide={this.handleToggle}>
+        <div className='modal-body'>
+          {this.renderRosterEditButton()} <br/><br/>
+          <form>
+                 Department <input type="text" name="course__deparment" value={course.department}/> <br/>
+                 Number <input type="text" name="course__number" value={course.course_number}/>
+                 Section <input type="text" name="course__section" value={course.section}/> <br/>
+                 Term <input type="text" name="course__term" value={course.term}/>
+                 Year <input type="text" name="course__year" value={course.year}/> <br/>
+                 Description <br/> <textarea type="text" name="course__description" value={course.description}/> <br/>
+          </form>
+        </div>
+        <div className='modal-footer'>
+          <Button bsStyle='primary'>Save changes</Button>
+          <Button onClick={this.handleToggle}>Close</Button>
+        </div>
+      </Modal>
+    );
   },
 
   renderInfoEditButton: function(){
